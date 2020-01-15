30be9d6244f9fe4d8c4f69dac7565d63e3d043eb
rafael pinon gonzalez <rafapinongonzalez@live.com.mx>
2020-01-15 10:22:34 -0700

    pagina de construmat

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>CONSTRUMAT</h1>
+        <h2>BIENVENIDO</h2>
+      </div>
+    );
+  }
+}
+
